510a564fda4e321ca76058694af69f2bfb093739
VadimKorobec <_s_e_t__@ukr.net>
2023-05-11 17:44:02 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,30 @@
 import { ContactForm } from 'components/ContactForm/ContactForm';
 import { ContactList } from 'components/ContactList/ContactList';
 import { Filter } from 'components/Filter/Filter';
+import { Component } from 'react';
+import initialContacts from '../data/contacts.json';
 
-export const App = () => {
-  return (
-    <div>
-      <h1>Phonebook</h1>
-      <ContactForm />
-      <h2>Contact</h2>
-      <Filter />
-      <ContactList />
-    </div>
-  );
-};
+export class App extends Component {
+  state = {
+    contacts: initialContacts,
+  };
+
+  addContact = (name, number) => {
+    console.log(name, number);
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, { name, number }],
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.addContact} />
+        <h2>Contact</h2>
+        <Filter />
+        <ContactList contacts={this.state.contacts} />
+      </div>
+    );
+  }
+}
