479c59766bb3992c0964e52562b95df1773cc099
VadimKorobec <_s_e_t__@ukr.net>
2023-05-11 16:37:13 +0300

    Create ContactList.jsx

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,11 @@
+import { ContactItem } from 'components/ContactItem/ContactItem';
+
+export const ContactList = () => {
+  return (
+    <>
+      <ul>
+        <ContactItem />
+      </ul>
+    </>
+  );
+};
